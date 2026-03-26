5d324b6fd933d419d204ed32d987e3ed49d5c921
Ngo Minh An <ngominhan666@gmail.com>
2026-03-26 09:38:04 +0700

    Created Index page

--- Server/ServerWeb/Models/PlaylistSong.cs
+++ Server/ServerWeb/Models/PlaylistSong.cs
@@ -2,12 +2,12 @@
 {
     public class PlaylistSong
     {
-        public int Id { get; set; }   // 👈 thêm dòng này
+        public int Id { get; set; } 
 
         public int PlaylistId { get; set; }
-        public int SongId { get; set; }
+        public Playlist Playlist { get; set; } 
 
-        public Playlist Playlist { get; set; }
+        public int SongId { get; set; }
         public Song Song { get; set; }
     }
 }
